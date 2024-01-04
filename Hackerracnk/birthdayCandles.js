a41e26c1156b33bef8ce6618c3a0f77d4ecfd27c
omoma ig <omomaighawosa440@gmail.com>
2024-01-04 15:43:22 +0100

    add solution for tallest candle challenge on hackerrank

--- Hackerracnk/birthdayCandles.js
+++ Hackerracnk/birthdayCandles.js
@@ -1,11 +1,12 @@
 /**
- You are in charge of the cake for a child's birthday. You have decided the cake will have one candle for each year of their total age. They will only be able to blow out the tallest of the candles. Count how many candles are tallest.
+ You are in charge of the cake for a child's birthday. You have decided the cake will have one candle for each year of their total age. 
+ They will only be able to blow out the tallest of the candles. tallestCandleArr how many candles are tallest.
 
 Example
 
 candles =[4,4,1,3]
 
-The maximum height candles are  units high. There are  of them, so return .
+The maximum height candles are 4 units high. There are 2 of them, so return 2.
 
 Function Description
 
@@ -15,23 +16,23 @@ birthdayCakeCandles has the following parameter(s):
 
 int candles[n]: the candle heights
 Returns
+-int: the number of candles that are tallest 
+*/
 
-int: the number of candles that are tallest 
- */
 
 function birthdayCakeCandles(candles) {
     // Write your code here
-    let tallestCandle = candles[0];
-    let count = 0;
-    for (let i = 0; i < candles.length; i++) {
-        if (candles[i] > tallestCandle) {
-            tallestCandle = candles[i];
-        }
-        if (candles[i] === tallestCandle) {
-            count++;
+    let sortedArr = candles.sort((a, b) => b - a)
+    let tallestCandle = sortedArr[0];
+    let tallestCandleArr = [];
+    for (let i = 0; i < sortedArr.length; i++) {
+        if (sortedArr[i] >= tallestCandle) {
+            tallestCandle = sortedArr[i];
+            tallestCandleArr.push(tallestCandle);
         }
     }
 
-    return count;
+    return tallestCandleArr.length;
 }
-console.log(birthdayCakeCandles([4, 2, 1, 4,2,2, 2]));
+let ans = birthdayCakeCandles([4, 2, 1, 4, 5, 8, 8, 2, 2, 2]);
+console.log(ans);
