227479aa3a4684330e00ebf775ec8657d7bae8bb
omoma ig <omomaighawosa440@gmail.com>
2024-01-06 14:05:10 +0100

    add solution to apples and oranges challenge

--- Hackerrank/applesAndOranges.js
+++ Hackerrank/applesAndOranges.js
@@ -1,13 +1,27 @@
-function kangaroo(x1, v1, x2, v2) {
-    // Write your code here
+/**LINK: https://www.hackerrank.com/challenges/apple-and-orange/problem?isFullScreen=true */
 
-    if(v2 > v1 && x2 > x1 ) {
-        return "NO"
-    } else {
-        return "YES"
-    }
-
+function countApplesAndOranges(s, t, a, b, apples, oranges) {
+   // Write your code here
+   let diffApples = apples.map((apple) => a + apple);
+   let diffOranges = oranges.map((orange) => b + orange);
+   let countA = 0;
+   let countO = 0;
+   while (s <= t) {
+      for (let i = 0; i < diffApples.length; i++) {
+         if (s === diffApples[i]) {
+            countA++;
+         }
+      }
+      for (let i = 0; i < diffOranges.length; i++) {
+         if (s === diffOranges[i]) {
+            countO++;
+         }
+      }
+      s++;
+   }
+  console.log(countA);
+  console.log(countO);
 }
 
-let ans = kangaroo(21, 6, 47, 3)
+let ans = countApplesAndOranges(7, 11, 5, 15, [-2, 2, 1], [5, -6]);
 console.log(ans);
